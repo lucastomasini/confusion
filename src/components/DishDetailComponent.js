b6a1c0251793378048e2fec8b4003b4fe31c33ca
lucastomasini <lucastomasini@hotmail.com>
2019-01-30 16:12:13 -0300

    Single Page Applications Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
     return (
@@ -42,21 +43,26 @@ function RenderComments({comments}) {
 }
 
 const DishDetail = (props) => {
-    const dish = props.dish;
-
-    if (dish != null) {
-        const comments = dish.comments;
-
+    if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
-                    <RenderDish dish={dish} />
-                    <RenderComments comments={comments} />
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
-        );
-    } 
-    else {
+        );    
+    } else {
         return(
             <div></div>
         );
